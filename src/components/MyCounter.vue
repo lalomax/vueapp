112a638f1029fb3bbf505c19564e342ff365c677
Lalomax <lalomax365@gmail.com>
2025-11-25 16:23:41 -0400

    perf: added function to MyCounter component

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ref } from 'vue';
 const count = ref(0);
+function increment () {
+    count.value++
+}
 </script>
 <template>
     <h3>MyCounter Component</h3>
     Reactive variable count: <b>{{ count }}</b>
     <br /><br />
-    <button @click="count++">count+1</button>
+    <button @click="increment">count+1</button>
 </template>
 <style scoped>
 h1 {
