37fc4188cbe119c5dd75d0fcda37bb6e60eb9532
Lalomax <lalomax365@gmail.com>
2025-11-25 16:03:58 -0400

    initial setup

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+<template>
+    <p class="red-text">MyCounter Component</p>
+</template>
+<style scoped>
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+}
+.red-text {
+    color: red;
+}
+</style>
